3832c0844e9b05af0f3af0f0258edf971f411b39
Michael Przybylski <mikep@gimmethebrain.net>
2021-02-19 15:07:12 -0800

    corrected typo in detect-setup-hidpi.sh, tweaked chroot WiFi configs

--- config/includes.chroot/sbin/detect-setup-hidpi.sh
+++ config/includes.chroot/sbin/detect-setup-hidpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 widest_mode="$(awk 'BEGIN{widest_mode=0}
-    match($0, /^U:([0-9]+)-[0-9]+p-0$/, a){if(a[1] > widest_mode){widest_mode=a[1]}}
+    match($0, /^U:([0-9]+)x[0-9]+p-0$/, a){if(a[1] > widest_mode){widest_mode=a[1]}}
     END{print widest_mode}' < /sys/class/graphics/fb0/modes)"
 
 # shellcheck disable=SC2086
