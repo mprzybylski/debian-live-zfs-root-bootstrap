d05b56275a69ee72ee928dedaab420a7a4454e9a
Michael Przybylski <mikep@gimmethebrain.net>
2021-02-19 10:59:10 -0800

    added detect-setup-hidpi.sh and additional chroot packages

---
+++ config/includes.chroot/sbin/detect-setup-hidpi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+widest_mode="$(awk 'BEGIN{widest_mode=0}
+    match($0, /^U:([0-9]+)-[0-9]+p-0$/, a){if(a[1] > widest_mode){widest_mode=a[1]}}
+    END{print widest_mode}' < /sys/class//sys/class/graphics/fb0/modes)"
+
+if [ "$widest_mode" -gt 1600 ]; then
+  # Use a bigger font on the console
+  setfont Uni3-Terminus32x16
+fi
+exit 0
