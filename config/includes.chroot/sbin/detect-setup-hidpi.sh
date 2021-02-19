06106a11320a1c9d37ce8a2e964916c3b65e2369
Michael Przybylski <mikep@gimmethebrain.net>
2021-02-19 13:56:12 -0800

    Added more wireless networking setup bits to chroot

--- config/includes.chroot/sbin/detect-setup-hidpi.sh
+++ config/includes.chroot/sbin/detect-setup-hidpi.sh
@@ -2,9 +2,10 @@
 
 widest_mode="$(awk 'BEGIN{widest_mode=0}
     match($0, /^U:([0-9]+)-[0-9]+p-0$/, a){if(a[1] > widest_mode){widest_mode=a[1]}}
-    END{print widest_mode}' < /sys/class//sys/class/graphics/fb0/modes)"
+    END{print widest_mode}' < /sys/class/graphics/fb0/modes)"
 
-if [ "$widest_mode" -gt 1600 ]; then
+# shellcheck disable=SC2086
+if [ $widest_mode -gt 1600 ]; then
   # Use a bigger font on the console
   setfont Uni3-Terminus32x16
 fi
